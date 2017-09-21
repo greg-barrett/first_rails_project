1473c178c90b9cd390a6332dbf4ce4551143dfb6
Gregory Barrett <gbarrett15@gmail.com>
2017-09-21 16:38:12 +0200

    Day 3- Form is complete

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
@@ -8,5 +9,32 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+    flash.notice = "Article '#{@article.title}' was created."
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash.notice = "Article '#{@article.title}' was deleted."
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' was updated."
+    redirect_to article_path(@article)
   end
 end
